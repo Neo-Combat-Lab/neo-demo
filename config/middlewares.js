fc3586c48c466c1a803fde3cef7213111e19745b
Sid <102430128+sgfractal@users.noreply.github.com>
2024-02-28 12:54:18 -0800

    1024 Remix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,23 +1,21 @@
-module.exports = {
-  middlewares: [
-    'strapi::logger',
-    'strapi::errors',
-    'strapi::security',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
-  ],
-  settings: {
-    parser: {
-      enabled: true,
-      multipart: true,
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::body',
+    config: {
       formidable: {
-        maxFileSize: 5 * 1024 * 1024 * 1024, // Set to 5GB
+        maxFileSize: 5 * 1024 * 1024 * 1024, // Sets the file size limit to 5GB
       },
+      multipart: true,
     },
   },
-};
+];
