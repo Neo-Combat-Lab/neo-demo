cb5ff83e348066660c3138bf76f6135f2b9ad5c7
Sid <102430128+sgfractal@users.noreply.github.com>
2024-02-28 12:24:51 -0800

    1024 upgrade

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  middlewares: [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ],
+  settings: {
+    parser: {
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 5 * 1024 * 1024 * 1024, // Set to 5GB
+      },
+    },
+  },
+};
